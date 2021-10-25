8b62a973166800185f9ff57dd87b616bfa953c18
kévin <kev4510@gmail.com>
2021-10-25 20:33:50 +0200

    modified burger menu

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(".burger").on("click", function(){
   $(".burger").toggleClass("close_btn");
   $(".navBar__links").toggleClass("open_menu");
+  $(".logo").toggleClass("hide_logo");
+  $("header").toggleClass("header_open")
 });
